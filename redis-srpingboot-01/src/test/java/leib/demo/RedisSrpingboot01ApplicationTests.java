0dfabe85462e923042cc7754a2e6bdddf7ecf53e
leib1368 <leib1368@gmail.com>
2020-05-16 15:24:09 +0800

    add redis-springboot

--- redis-srpingboot-01/src/test/java/leib/demo/RedisSrpingboot01ApplicationTests.java
+++ redis-srpingboot-01/src/test/java/leib/demo/RedisSrpingboot01ApplicationTests.java
@@ -1,13 +1,30 @@
 package leib.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import leib.demo.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class RedisSrpingboot01ApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        //操作value 类似String
+        redisTemplate.opsForValue().set("hi","leib");
+    }
+
+    @Test
+    public void test() {
+        User user = new User("法外狂徒张三",18);
+
+        redisTemplate.opsForValue().set("user",user);
+        System.out.println(redisTemplate.opsForValue().get("user"));
     }
 
 }
